70f25105619a0cbb838abd6cc68529a5de0a2858
Ignacio-Montaldi <ignaciomontaldi@gmail.com>
2022-01-06 17:23:49 -0300

    Animaciones carrito, estructuras de archivos, instalación de EJS

--- src/app.js
+++ src/app.js
@@ -3,14 +3,15 @@ const app = express();
 const path = require("path");
 
 app.set("port", 3000);
+app.set("view engine", "ejs");
 
 app.listen(app.get("port"), () => console.log("Servidor Corriendo"));
 
-app.get('/', (req, res) => { res.sendFile(path.join(__dirname, './views', 'index.html')) });
-app.get('/homesecundario', (req, res) => { res.sendFile(path.join(__dirname, './views', 'home_secundario.html')) });
-app.get('/productCart', (req, res) => { res.sendFile(path.join(__dirname, './views', 'productCart.html')) });
-app.get('/login', (req, res) => { res.sendFile(path.join(__dirname, './views', 'login.html')) });
-app.get('/register', (req, res) => { res.sendFile(path.join(__dirname, './views', 'register.html')) });
-app.get('/productDetail', (req, res) => { res.sendFile(path.join(__dirname, './views', 'productDetail.html')) });
+app.get('/', (req, res) => { res.render(path.join(__dirname, "./views", "users", "index")) });
+app.get('/homesecundario', (req, res) => { res.render(path.join(__dirname, './views',"users", 'home_secundario')) });
+app.get('/productCart', (req, res) => { res.render(path.join(__dirname, './views',"products", 'productCart')) });
+app.get('/login', (req, res) => { res.render(path.join(__dirname, './views',"users", 'login')) });
+app.get('/register', (req, res) => { res.render(path.join(__dirname, './views',"users", 'register')) });
+app.get('/productDetail', (req, res) => { res.render(path.join(__dirname, './views',"products", 'productDetail')) });
 
 app.use(express.static('./public'));
