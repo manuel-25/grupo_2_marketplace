c526fa5a44bbecd58b12275ae0e97f64cf1f9983
naalonso29 <naalonso29@gmail.com>
2022-01-19 18:56:14 -0300

    users.json

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,20 +1,36 @@
 const path = require('path');
 
 const controller = {
-    carrito: (req,res) =>{
+    lista: (req, res) => {
+        res.render(path.resolve(__dirname, "..", "views", "products", "list"))
+    },
+
+    carrito: (req, res) => {
         res.render(path.resolve(__dirname, "..", "views", "products", "cart"))
     },
 
-    detalle: (req, res) =>{
+    detalle: (req, res) => {
         res.render(path.resolve(__dirname, "..", "views", "products", "detail"))
     },
 
-    crearProducto: (req, res) =>{
+    crearProducto: (req, res) => {
         res.render(path.resolve(__dirname, "..", "views", "products", "create"))
     },
 
-    modificarProducto: (req, res) =>{
+    modificarProducto: (req, res) => {
         res.render(path.resolve(__dirname, "..", "views", "products", "modify"))
+    },
+
+    crear: (req, res) => {
+
+    },
+
+    editar: (req, res) => {
+
+    },
+
+    borrar: (req, res) => {
+
     }
 }
 
--- src/routes/products.js
+++ src/routes/products.js
@@ -4,10 +4,18 @@ const products = require('../controllers/productsController');
 
 router.get("/cart", products.carrito)
 
-router.get("/detail", products.detalle)
+router.get("/:id", products.detalle)
 
 router.get("/create", products.crearProducto)
 
-router.get("/modify", products.modificarProducto);
+router.get("/:id/edit", products.modificarProducto);
+
+router.get("/", products.lista);
+
+//router.post("", products.crear);
+
+router.put("/:id", products.editar)
+
+router.delete("/:id", products.borrar)
 
 module.exports = router;
